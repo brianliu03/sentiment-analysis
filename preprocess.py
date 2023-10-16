3d20b403270ca19564ee362db31cfb16e2bd31c0
Brian Liu <64610885+brianliu03@users.noreply.github.com>
2023-10-16 10:13:37 +0100

    Update preprocess

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,8 @@ import torch, argparse
 from datasets import load_dataset
 from transformers import AutoTokenizer
 
+
+# define command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, default="imdb",
                     help="Path to repository of the dataset to use.")
@@ -11,7 +13,13 @@ parser.add_argument("--test_samples", type=int, default=300,
                     help="Number of testing samples to use.")
 args = parser.parse_args()
 
+
+# load pre-trained tokenizer
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-cased")
 
 def preprocess(examples):
     return tokenizer(examples["text"], truncation=True)
+
+dataset = load_dataset(args.dataset)
+dataset = dataset.map(preprocess, batched=True)
+
